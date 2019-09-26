3c319c61961e68defc996103508d99235d7459c5
Nicolas Vargas Quiroga <jelboy33@gmail.com>
2019-09-25 19:00:23 -0500

    fghgh

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, Platform } from 'ionic-angular';
+import { NavController, AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {}
 
   showPlatform() {
-    let text = 'I run on: ' + this.platform.platforms();
-    let alert = this.alertCtrl.create({
-      title: 'My Home',
-      subTitle: text,
-      buttons: ['Ok']
-    });
-    alert.present();
+    console.log('entro');
   }
 
 }
